05da3c0efeae46e3ab13ef4dea95427ed33b892a
Alex Bashara <alexbashara2002@gmail.com>
2025-05-27 23:39:31 -0700

    Added application code

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -23,6 +23,92 @@
 #define LED_GPIO                              (GPIOA)
 #define USB_BUF_LEN                           (128)
 
+// LoRa Packet IDs
+#define LORA_SUSPENSION_ID          (0x01) // 50 Hz
+#define LORA_GPS_ID                 (0x02) // 25 Hz
+#define LORA_ENGINE_ID              (0x03) // 20 Hz
+#define LORA_BRAKES_ACCEL_ID        (0x04) // 10 Hz
+#define LORA_TEMPERATURE_ID         (0x05) // 1 Hz
+
+/* Data Structures  ---------------------------------------------------------*/
+/**
+ * @brief 50 Hz packet with ID 0x01
+ * @note  Contains Front and Rear Suspension Potentiometer values
+ */
+typedef struct {
+
+  uint16_t FrontPot;                // Front Right Suspension Damper
+  uint16_t RearPot;                 // Rear Right Suspension Damper
+
+} LoRa_Suspension_Packet;
+
+/**
+ * @brief 25 Hz packet with ID 0x02
+ * @note  Contains GPS Latitude, Longitude, and Speed
+ */
+typedef struct {
+
+  int32_t latGPS;                   // Latitude GPS
+  int32_t longGPS;                  // Longitude GPS
+  int32_t Speed;                    // Vehicle GPS Speed
+
+} LoRa_GPS_Packet;
+
+/**
+ * @brief 20 Hz packet with ID 0x03
+ * @note  Contains Engine RPM, Throttle Position, Steering Angle, and Brake Pressure
+ */
+typedef struct {
+
+  uint16_t BrakePressure;           // Brake Pressure
+  uint16_t ThrottleADC;             // Analog Throttle Position
+  uint16_t Steering;                // Steering Angle
+  uint16_t RPM;                     // Engine RPM
+  uint16_t ThrottlePosSensor;       // Throttle Position from ECU
+  uint16_t Lambda;                  // Lambda
+
+} LoRa_Engine_Data_Packet;
+
+/**
+ * @brief 10 Hz packet with ID 0x04
+ * @note  Contains Oil Pressure, Front and Rear Brake Temp, and Accelerometer values
+ */
+typedef struct {
+
+  uint16_t OilPressure;             // Oil Pressure
+  uint16_t FrontBrakeTemp;          // Front Right Brake Temp (F)
+  uint16_t RearBrakeTemp;           // Rear Right Brake Temp (F)
+  uint16_t AccelX;                  // Accelerometer X Axis
+  uint16_t AccelZ;                  // Accelerometer Z Axis
+  uint16_t AccelY;                  // Accelerometer Y Axis
+
+} LoRa_Brakes_Accel_Packet;
+
+/**
+ * @brief 1 Hz packet with ID 0x05
+ * @note  Contains Air and Coolant Temp
+ */
+typedef struct {
+
+  uint16_t AirTemp;                 // Air Temp (F)
+  uint16_t CoolTemp;                // Coolant Temp (F)
+
+} LoRa_Temperature_Packet;
+
+/**
+ * @brief Telemetry Struct to hold all Telemetry Data
+ * @note  Anything not in a LoRa packet should be in this struct
+ */
+typedef struct {
+  // LoRa Packets
+  LoRa_Suspension_Packet Suspension_Packet;           // 50 Hz
+  LoRa_GPS_Packet GPS_Packet;                         // 25 Hz
+  LoRa_Engine_Data_Packet Engine_Data_Packet;         // 20 Hz
+  LoRa_Brakes_Accel_Packet Brakes_Accel_Packet;       // 10 Hz
+  LoRa_Temperature_Packet Temperature_Packet;         // 1 Hz
+
+} Telemetry;
+
 /* Functions prototypes -----------------------------------------------------*/
 
 /**
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,21 +10,74 @@
 #include "main.h"
 
 /* Global Variables ---------------------------------------------------------*/
+uint8_t packetRecv;
+uint8_t LoRaPacketBuf[128];
+uint8_t LoRaPacketLen;
 uint8_t usbTxBuf[USB_BUF_LEN];
 uint8_t usbBufLen;
+
+Telemetry telemetry;
 
 /* Function Implementation --------------------------------------------------*/
 
 int main(void) {
-  HAL_Init(); // Initialize the HAL Library
+  HAL_Init(); // Used for USB
   SystemClock_Config(); // Sets to 48MHz using HSI48
   GPIO_Init();
   MX_USB_DEVICE_Init();
 
   while (1) {
-    usbBufLen = snprintf((char*)usbTxBuf, USB_BUF_LEN, "Test!\r\n");
-    CDC_Transmit_FS(usbTxBuf, usbBufLen); // Transmit over USB CDC
-    Toggle_Pin(LED_GPIO, LED_PIN); // Toggle the LED pin
+    if (packetRecv) {
+      Toggle_Pin(LED_GPIO, LED_PIN); // Toggle the LED pin
+      packetRecv = 0;
+      // Lora_Receive(LoRaPacketBuf, &LoRaPacketLen); // Receive LoRa packet
+      uint8_t LoRaPacketID = LoRaPacketBuf[0]; // Get the packet ID
+
+      switch(LoRaPacketID) {
+        // Start at buffer index 1 to skip the ID byte
+        case LORA_SUSPENSION_ID: {
+          telemetry.Suspension_Packet = *(LoRa_Suspension_Packet*)LoRaPacketBuf[1];
+          break;
+        }
+
+        case LORA_GPS_ID: {
+          LoRa_GPS_Packet* gpsPacket = (LoRa_GPS_Packet*)LoRaPacketBuf[1];
+          break;
+        }
+
+        case LORA_ENGINE_ID: {
+          LoRa_Engine_Data_Packet* enginePacket = (LoRa_Engine_Data_Packet*)LoRaPacketBuf[1];
+          break;
+        }
+
+        case LORA_BRAKES_ACCEL_ID: {
+          LoRa_Brakes_Accel_Packet* brakesAccelPacket = (LoRa_Brakes_Accel_Packet*)LoRaPacketBuf[1];
+          break;
+        }
+
+        case LORA_TEMPERATURE_ID: {
+          LoRa_Temperature_Packet* tempPacket = (LoRa_Temperature_Packet*)LoRaPacketBuf[1];
+          break;
+        }
+
+        default:
+          usbBufLen = snprintf((char*)usbTxBuf, USB_BUF_LEN, "Error: Unknown Packet ID %02X\r\n", LoRaPacketID);
+          CDC_Transmit_FS(usbTxBuf, usbBufLen);
+          break;
+      }
+
+      // Transmit CSV formated data
+      usbBufLen = snprintf((char*)usbTxBuf, USB_BUF_LEN, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d,\r\n",
+        telemetry.Suspension_Packet.FrontPot, telemetry.Suspension_Packet.RearPot, telemetry.GPS_Packet.latGPS,
+        telemetry.GPS_Packet.longGPS, telemetry.GPS_Packet.Speed, telemetry.Engine_Data_Packet.BrakePressure,
+        telemetry.Engine_Data_Packet.ThrottleADC, telemetry.Engine_Data_Packet.Steering, telemetry.Engine_Data_Packet.RPM,
+        telemetry.Engine_Data_Packet.ThrottlePosSensor, telemetry.Engine_Data_Packet.Lambda,
+        telemetry.Brakes_Accel_Packet.OilPressure, telemetry.Brakes_Accel_Packet.FrontBrakeTemp,
+        telemetry.Brakes_Accel_Packet.RearBrakeTemp, telemetry.Brakes_Accel_Packet.AccelX, telemetry.Brakes_Accel_Packet.AccelZ,
+        telemetry.Brakes_Accel_Packet.AccelY, telemetry.Temperature_Packet.AirTemp, telemetry.Temperature_Packet.CoolTemp
+      );
+      CDC_Transmit_FS(usbTxBuf, usbBufLen); // Transmit over USB CDC
+    }
   }
 }
 
